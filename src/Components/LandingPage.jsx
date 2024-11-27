bae5042b6696805540332b69494b7c45b126f077
Matthew Pantel <79867801+TypeErrorDev@users.noreply.github.com>
2024-11-27 08:18:29 -0800

    created env variables, and started to connect the supabase db

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -1,11 +1,33 @@
-import CTA from "./CTA";
+import { Link } from "react-router-dom";
+// import CTA from "./CTA";
 import LandingNav from "./LandingNav";
+import Login from "./Login";
 
 const LandingPage = () => {
   return (
     <div>
       <LandingNav />
-      <CTA />
+      <div className="flex flex-col justify-center items-center">
+        <h1 className="text-4xl font-bold mt-16">Track Projects with</h1>
+        <span className="text-4xl font-bold text-purple-600">Confidence</span>
+        <div className="flex justify-center items-center text-wrap w-[400px] text-center mt-5 text-lg">
+          <p className="text-slate-500 leading-loose">
+            Streamline your workflow, boost productivity, and never miss a
+            deadline. The ultimate project tracking solution for developers and
+            teams.
+          </p>
+        </div>
+        <div className="flex flex-col justify-center items-center">
+          <Link to="/login" element={<Login />}>
+            <button className="bg-purple-600 shadow-md text-white font-semibold h-9 w-96 my-4 rounded-md hover:transition-transform hover:scale-[1.02] hover:bg-slate-800">
+              Get Started Free <span className="font-bold h-4 w-3">→</span>
+            </button>
+          </Link>
+          <button className="border shadow-md font-semibold h-9 w-96 hover:transition-transform hover:scale-[1.02] hover:bg-[#faf9f9] rounded-md">
+            View on Github
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
